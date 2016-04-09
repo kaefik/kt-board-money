3d37f778777d35259acf2acce4d0eaf83600ee20
Ilnur Sayfutdinov <ilnursoft@gmail.com>
2016-04-10 00:58:44 +0300

    add

--- src/kt-board-money.kt
+++ src/kt-board-money.kt
@@ -9,6 +9,10 @@ class Kurs {
     var pokupka: Float? = null  //  покупка валюты (покупает банк)
     var prodaja: Float? = null  //  продажа валюты  (продает банк)
 
+    //парсинг банка с именем nbank, возвращает false - если ошибка парсинга
+    fun parsebank(nbank: String?): Boolean {
+        return true
+    }
 
     fun print() {
         println("namebank= $namebank")
@@ -18,10 +22,11 @@ class Kurs {
     }
 }
 
-fun List<Kurs>.printboard() {
-    println("имя банка \t валюта \t покупка \t продажа \n")
+// распечатка таблицы металлов на экран
+fun MutableList<Kurs>.printboard() {
+    println("имя банка \t валюта \t покупка \t продажа ")
     for (i in this) {
-        println("\t $i.namebank")
+        println(" ${i.namebank} \t ${i.valuta}  \t  ${i.pokupka}  \t  ${i.prodaja}")
     }
 }
 
@@ -29,12 +34,13 @@ fun main(args: Array<String>) {
     println("Starting kt-board-money\n")
 
     var metalkurs: Kurs
-    var arraykurs: List<Kurs>
-    arraykurs = listOf()
+    var boardkurs: MutableList<Kurs>  // доска валют
+    boardkurs = mutableListOf()
     metalkurs = Kurs()
 
-    metalkurs.print()
-    arraykurs.printboard()
+    //    metalkurs.print()
+    boardkurs.add(metalkurs)
+    boardkurs.printboard()
 
     //    println("\nСписок металлов\n $arraykurs")
     println("Stopping kt-board-money")
