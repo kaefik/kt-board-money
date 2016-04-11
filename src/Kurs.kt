1c0c109e224814d9e211420ca9ce6b6a74c743f9
Ilnur Sayfutdinov <ilnursoft@gmail.com>
2016-04-11 23:09:08 +0300

    fix

--- src/Kurs.kt
+++ src/Kurs.kt
@@ -1,20 +1,15 @@
 /**
  * Created by oilnur on 10.04.16.
  */
+import org.jsoup.Jsoup
+import java.net.URL
+import java.nio.charset.Charset
+
 data class Kurs (var namebank: String? = null,var valuta: String? = null,var pokupka: Float? = null,var prodaja: Float? = null ){
 //    var namebank: String? = null // название банка
 //    var valuta: String? = null  //  название валюты
 //    var pokupka: Float? = null  //  покупка валюты (покупает банк)
 //    var prodaja: Float? = null  //  продажа валюты  (продает банк)
-
-    //парсинг банка с именем nbank, возвращает false - если ошибка парсинга
-    fun parsebank(nbank: String?): Boolean {
-        when(nbank) {
-            "SBRF" -> println("parse SBRF")
-            else   -> println("unknown parse")
-        }
-        return true
-    }
 
     fun print() {
         println("namebank= $namebank")
@@ -22,4 +17,48 @@ data class Kurs (var namebank: String? = null,var valuta: String? = null,var pok
         println("pokupka=  $pokupka")
         println("prodaja=  $prodaja")
     }
+}
+
+// распечатка таблицы металлов на экран
+fun MutableList<Kurs>.printboard() {
+    println("имя банка \t валюта \t покупка \t продажа ")
+    for (i in this) {
+        println(" ${i.namebank} \t ${i.valuta}  \t  ${i.pokupka}  \t  ${i.prodaja}")
+    }
+}
+
+// парсинг сбербанка котировки металлов
+fun MutableList<Kurs>.parsemetalSbrf() {
+    var metal: Kurs
+    val linkbank = URL("http://data.sberbank.ru/tatarstan/ru/quotes/metal/?base=beta")
+    var CHARSETSBRF = Charset.forName("windows-1251")
+    val htmlpage: String = linkbank.readText(CHARSETSBRF)
+    // парсинг тэгов таблицы
+    val doc = Jsoup.parse(htmlpage)
+    val tables = doc.getElementsByTag("table")
+    var valmetal: String = ""
+    for (i in tables) {
+        if (i.attr("class") == "table3_eggs4") {
+            valmetal = i.text()
+            break
+        }
+    }
+
+    val arraymetal = valmetal.split(" ")
+    metal=Kurs("SBRF","Au")
+    metal.pokupka = arraymetal[2].replace(",", ".").toFloat()
+    metal.prodaja = arraymetal[3].replace(",", ".").toFloat()
+    this.add(metal)
+    metal=Kurs("SBRF","Ag")
+    metal.pokupka = arraymetal[4].replace(",", ".").toFloat()
+    metal.prodaja = arraymetal[5].replace(",", ".").toFloat()
+    this.add(metal)
+    metal=Kurs("SBRF","Pt")
+    metal.pokupka = arraymetal[6].replace(",", ".").toFloat()
+    metal.prodaja = arraymetal[7].replace(",", ".").toFloat()
+    this.add(metal)
+    metal=Kurs("SBRF","Pd")
+    metal.pokupka = arraymetal[8].replace(",", ".").toFloat()
+    metal.prodaja = arraymetal[9].replace(",", ".").toFloat()
+    this.add(metal)
 }
--- src/kt-board-money.kt
+++ src/kt-board-money.kt
@@ -2,55 +2,9 @@
  * kt-board-money - доска металлов и валют из банков
  * Created by oilnur on 09.04.16.
  */
-import java.net.URL
+
 import java.io.File
 import java.nio.charset.Charset
-import org.jsoup.*
-
-
-// распечатка таблицы металлов на экран
-fun MutableList<Kurs>.printboard() {
-    println("имя банка \t валюта \t покупка \t продажа ")
-    for (i in this) {
-        println(" ${i.namebank} \t ${i.valuta}  \t  ${i.pokupka}  \t  ${i.prodaja}")
-    }
-}
-
-// парсинг сбербанка котировки металлов
-fun MutableList<Kurs>.parsemetalSbrf() {
-    var metal: Kurs
-    val linkbank = URL("http://data.sberbank.ru/tatarstan/ru/quotes/metal/?base=beta")
-    var CHARSETSBRF = Charset.forName("windows-1251")
-    val htmlpage: String = linkbank.readText(CHARSETSBRF)
-    // парсинг тэгов таблицы
-    val doc = Jsoup.parse(htmlpage)
-    val tables = doc.getElementsByTag("table")
-    var valmetal: String = ""
-    for (i in tables) {
-        if (i.attr("class") == "table3_eggs4") {
-            valmetal = i.text()
-            break
-        }
-    }
-
-    val arraymetal = valmetal.split(" ")
-    metal=Kurs("SBRF","Au")
-    metal.pokupka = arraymetal[2].replace(",", ".").toFloat()
-    metal.prodaja = arraymetal[3].replace(",", ".").toFloat()
-    this.add(metal)
-    metal=Kurs("SBRF","Ag")
-    metal.pokupka = arraymetal[4].replace(",", ".").toFloat()
-    metal.prodaja = arraymetal[5].replace(",", ".").toFloat()
-    this.add(metal)
-    metal=Kurs("SBRF","Pt")
-    metal.pokupka = arraymetal[6].replace(",", ".").toFloat()
-    metal.prodaja = arraymetal[7].replace(",", ".").toFloat()
-    this.add(metal)
-    metal=Kurs("SBRF","Pd")
-    metal.pokupka = arraymetal[8].replace(",", ".").toFloat()
-    metal.prodaja = arraymetal[9].replace(",", ".").toFloat()
-    this.add(metal)
-}
 
 //сохранение строки htmlpage в файле с именем namefile
 fun SaveFile(namefile: String,htmlpage:String) {
